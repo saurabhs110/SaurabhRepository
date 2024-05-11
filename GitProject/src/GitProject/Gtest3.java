e2f06be568cc03864d0d791736ebc430a095e9ac
Admin <Admin@DESKTOP-V4JO8I8>
2024-05-11 22:52:07 +0530

    test commite

---
+++ GitProject/src/GitProject/Gtest3.java
@@ -0,0 +1,16 @@
+package GitProject;
+
+
+	import java.util.Scanner;
+	
+	public class Gtest3 {
+	 public static void main(String [] args){
+	   String s1="TestAutomation";  
+
+	 
+	       System.out.println("string length is: "+s1.length());//14 is the length of TestAutomation string  
+	        }
+	       
+	   
+	    
+	}
