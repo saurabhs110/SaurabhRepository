9161b24cbd9317dcb803f6208c280bff27d9023f
Admin <Admin@DESKTOP-V4JO8I8>
2024-05-11 21:46:07 +0530

    Project push to github

---
+++ GitProject/src/GitProject/Gtest1.java
@@ -0,0 +1,27 @@
+package GitProject;
+
+import java.util.Scanner;
+
+public class Gtest1 {
+  
+	
+		public static void main(String[] args) {
+			 
+		
+		        // TODO Auto-generated method stub
+		        int x, z, temp;
+		        System.out.println("Enter x and Z");
+		        Scanner in = new Scanner(System.in);
+		        x = in.nextInt();
+		        z = in.nextInt();
+		        System.out.println("Befores Swapping: " + x + z);
+		        temp = x;
+		        x = z;
+		        z = temp;
+		        System.out.println("After Swapping: " + x + z);
+		         
+		    } 
+			
+}
+
+
